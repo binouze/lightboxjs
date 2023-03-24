c8598ea60a24897b992f3e0038835d1dacac0cdc
Benjamin BOUFFIER <benjamin.bouffier@gmail.com>
2023-03-24 18:20:10 +0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lightboxjs
 a lightweight javascript library for opening links in lightboxed iframe
+
+See the index.html for exemple usage
---
+++ src/lightboxjs.js
@@ -0,0 +1,212 @@
+/**
+ * Create a lightbox from a link element or any other element with a data-url attribute
+ * @param elem
+ * @constructor
+ */
+let LightboxJS = function( elem )
+{
+    // initialize if not already done
+    if( !LightboxJS.isInit )
+    {
+        LightboxJS.init();
+        LightboxJS.isInit = true;
+    }
+
+    this.trigger  = elem;
+    this.href     = elem.getAttribute('href') ?? elem.dataset.url ?? null;
+    this.width    = elem.dataset.width  ?? 1000;
+    this.height   = elem.dataset.height ?? 600;
+
+    const closeWithOverlayData = (elem.dataset.closeWithOverlay ?? 1);
+    this.closeWithOverlay = closeWithOverlayData !== 0 && closeWithOverlayData !== '0' && closeWithOverlayData !== 'false';
+
+    if( this.href != null )
+    {
+        const _this = this;
+
+        // catch click events to start opening links in iframes
+        this.trigger.addEventListener('click', (e) =>
+        {
+            e.preventDefault();
+            _this.open();
+        });
+    }
+    else
+    {
+        console.error('[LightboxJS] this element does not contains url information');
+    }
+
+    LightboxJS.current = this;
+};
+
+/**
+ * Create the lighbox elements
+ */
+LightboxJS.init = function ()
+{
+    LightboxJS.el       = document.createElement('div');
+    LightboxJS.content  = document.createElement('div');
+    LightboxJS.body     = document.createElement('div');
+    LightboxJS.spinner  = document.createElement('div');
+    LightboxJS.overlay  = document.createElement('div');
+    LightboxJS.closebtn = document.createElement('div');
+    LightboxJS.frame    = document.createElement('iframe');
+
+    LightboxJS.closebtn.innerHTML = "&times;";
+
+    LightboxJS.el.classList.add('lightboxjs');
+    LightboxJS.overlay.classList.add('overlay');
+    LightboxJS.spinner.classList.add('spinner');
+    LightboxJS.content.classList.add('content');
+    LightboxJS.closebtn.classList.add('close');
+    LightboxJS.body.classList.add('body');
+
+    LightboxJS.el.appendChild(this.overlay);
+    LightboxJS.el.appendChild(this.closebtn);
+    LightboxJS.el.appendChild(this.spinner);
+    LightboxJS.content.appendChild(this.body);
+    LightboxJS.el.appendChild(this.content);
+    LightboxJS.body.append(this.frame);
+    document.body.appendChild(this.el);
+
+    LightboxJS.frame.onload = () =>
+    {
+        if( !LightboxJS.isOpen() )
+            return;
+
+        LightboxJS.spinner.classList.remove('show');
+        LightboxJS.content.classList.add('loaded');
+        LightboxJS.closebtn.classList.add('show');
+        LightboxJS.closebtn.classList.add('open');
+
+        const ff = () =>
+        {
+            LightboxJS.closebtn.classList.remove('show');
+
+            LightboxJS.closebtn.removeEventListener('webkitAnimationEnd', ff, false);
+            LightboxJS.closebtn.removeEventListener('mozAnimationEnd',    ff, false);
+            LightboxJS.closebtn.removeEventListener('msAnimationEnd',     ff, false);
+            LightboxJS.closebtn.removeEventListener('animationend',       ff, false);
+        }
+
+        LightboxJS.closebtn.addEventListener('webkitAnimationEnd', ff, false);
+        LightboxJS.closebtn.addEventListener('mozAnimationEnd',    ff, false);
+        LightboxJS.closebtn.addEventListener('msAnimationEnd',     ff, false);
+        LightboxJS.closebtn.addEventListener('animationend',       ff, false);
+    };
+
+    const closeWithOverlay = () =>
+    {
+        if( LightboxJS.current.closeWithOverlay )
+            LightboxJS.close();
+    }
+
+    const closeWithBtn = () =>
+    {
+        LightboxJS.close();
+    }
+
+    const f = () =>
+    {
+        if( LightboxJS.isOpen() )
+            return;
+
+        LightboxJS.frame.src = 'about:blank';
+        LightboxJS.el.classList.remove('show');
+        LightboxJS.closebtn.classList.remove('show');
+        LightboxJS.closebtn.classList.remove('open');
+    };
+
+    LightboxJS.overlay.addEventListener('click',  closeWithOverlay );
+    LightboxJS.closebtn.addEventListener('click', closeWithBtn );
+
+    // listen for lightboxjs-close-me messages
+    window.addEventListener( "message", (event) =>
+        {
+            if( event.data === "lightboxjs-close-me" )
+                LightboxJS.close();
+        },
+        false
+    );
+
+    this.el.addEventListener('transitionend',       f, false);
+    this.el.addEventListener('webkitTransitionEnd', f, false);
+    this.el.addEventListener('mozTransitionEnd',    f, false);
+    this.el.addEventListener('msTransitionEnd',     f, false);
+};
+
+/**
+ * Load the url in the iframe
+ */
+LightboxJS.loadIframe = function (href)
+{
+    // show the spinner
+    LightboxJS.spinner.classList.add('show');
+    // hide the content until the url is loaded
+    LightboxJS.content.classList.remove('loaded');
+    // hide the close button until loaded
+    LightboxJS.closebtn.classList.remove('show');
+    LightboxJS.closebtn.classList.remove('open');
+    // set the iframe url
+    LightboxJS.frame.src = href;
+};
+
+/**
+ * Open the lightbox
+ */
+LightboxJS.prototype.open = function ()
+{
+    // be sure the frame has the right dimensions
+    LightboxJS.setDimensions( this.width, this.height );
+    // start loading url
+    LightboxJS.loadIframe(this.href);
+
+    // show the lightbox parent element
+    LightboxJS.el.classList.add('show');
+    LightboxJS.el.offsetHeight; // without this, the css transition don't work
+    LightboxJS.el.classList.add('open');
+
+    // update the class of the overlay to show or not the clickable pointer
+    if( !this.closeWithOverlay )
+        LightboxJS.overlay.classList.remove('clickable');
+    else
+        LightboxJS.overlay.classList.add('clickable');
+
+    // enregistrer l'instance en cours d'ouverture
+    LightboxJS.current = this;
+};
+
+/**
+ * Close the lightbox
+ */
+LightboxJS.close = function ()
+{
+    // hide the lightbox parent element
+    // this starts the hide transition
+    LightboxJS.el.classList.remove('open');
+}
+
+LightboxJS.isOpen = function ()
+{
+    // true if the lightbox is open
+    return LightboxJS.el.classList.contains('open');
+};
+
+// define the with and height of the iframe
+LightboxJS.setDimensions = function (w, h)
+{
+    //this.width  = w;
+    //this.height = h;
+    LightboxJS.content.style.width  = w + 'px';
+    LightboxJS.content.style.height = h + 'px';
+};
+
+
+// on document is loaded, transform elements with lightboxjs-link class into lighboxjs elements
+document.addEventListener("DOMContentLoaded", () =>
+{
+    document.querySelectorAll('.lightboxjs-link').forEach( (el) =>
+    {
+        el.lightbox = new LightboxJS(el);
+    });
+});
